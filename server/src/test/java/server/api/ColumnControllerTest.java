1a44306ebba1c584bfdd5700f48a2e539f942383
Tejas Kochar <T.Kochar@student.tudelft.nl>
2023-03-23 09:32:33 +0100

    tested removeColumn endpoint as well

--- server/src/test/java/server/api/ColumnControllerTest.java
+++ server/src/test/java/server/api/ColumnControllerTest.java
@@ -1,6 +1,5 @@
 package server.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import commons.Board;
 import commons.Column;
@@ -65,5 +64,99 @@ public class ColumnControllerTest {
         assertEquals(initialBoard, finalBoard);
     }
 
+    @Test
+    public void addColumnToBoardWithoutPasswordTest() throws Exception {
 
+        Column addedColumn = new Column("Column 1", 1, new TreeSet<>());
+
+        Board initialBoard = new Board("joinkey", "Board 1", null, new TreeSet<>(), new Timestamp(12345L));
+        Board finalBoard = new Board("joinkey", "Board 1", null, new TreeSet<>(), new Timestamp(12345L));
+        finalBoard.addColumn(addedColumn);
+
+        when(boardService.getBoardWithKeyAndPassword(anyString(), anyString())).thenReturn(initialBoard);
+        when(boardService.saveBoard(any(Board.class))).thenReturn(finalBoard);
+
+        // Perform the request
+        this.mockMvc.perform(post("/columns/joinkey/create/Column 1").param("index", "1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(null)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(addedColumn)));
+
+        assertEquals(addedColumn, initialBoard.getColumns().first());
+        assertEquals(initialBoard, finalBoard);
+    }
+
+    @Test
+    public void removeColumnFromBoardWithPasswordTest() throws Exception {
+
+        Column toBeRemovedColumn = new Column("Column 1", 1, new TreeSet<>());
+
+        Board initialBoard = new Board("joinkey", "Board 1", "password", new TreeSet<>(), new Timestamp(12345L));
+        Board finalBoard = new Board("joinkey", "Board 1", "password", new TreeSet<>(), new Timestamp(12345L));
+        initialBoard.addColumn(toBeRemovedColumn);
+
+        when(boardService.getBoardWithKeyAndPassword(anyString(), anyString())).thenReturn(initialBoard);
+        when(boardService.saveBoard(any(Board.class))).thenReturn(finalBoard);
+
+        // Perform the request
+        this.mockMvc.perform(post("/columns/joinkey/remove/Column 1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString("password")))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(toBeRemovedColumn)));
+
+        assertEquals(0, initialBoard.getColumns().size());
+        assertEquals(initialBoard, finalBoard);
+    }
+
+    @Test
+    public void removeColumnFromBoardWithoutPasswordTest() throws Exception {
+
+        Column toBeRemovedColumn = new Column("Column 1", 1, new TreeSet<>());
+
+        Board initialBoard = new Board("joinkey", "Board 1", null, new TreeSet<>(), new Timestamp(12345L));
+        Board finalBoard = new Board("joinkey", "Board 1", null, new TreeSet<>(), new Timestamp(12345L));
+        initialBoard.addColumn(toBeRemovedColumn);
+
+        when(boardService.getBoardWithKeyAndPassword(anyString(), anyString())).thenReturn(initialBoard);
+        when(boardService.saveBoard(any(Board.class))).thenReturn(finalBoard);
+
+        // Perform the request
+        this.mockMvc.perform(post("/columns/joinkey/remove/Column 1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(null)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(toBeRemovedColumn)));
+
+        assertEquals(0, initialBoard.getColumns().size());
+        assertEquals(initialBoard, finalBoard);
+    }
+
+    /**
+     * The removeColumn endpoint tries to remove the specified column, but if the column does not exist,
+     * nothing should happen.
+     * @throws Exception JsonProcessingException thrown by ObjectMapper.writeValueAsString and also Exception thrown by MockMvc.perform
+     */
+    @Test
+    public void removeColumnFromEmptyBoardTest() throws Exception {
+
+        Column toBeRemovedColumn = new Column("Column 1", 1, new TreeSet<>());
+
+        Board initialBoard = new Board("joinkey", "Board 1", "password", new TreeSet<>(), new Timestamp(12345L));
+        Board finalBoard = new Board("joinkey", "Board 1", "password", new TreeSet<>(), new Timestamp(12345L));
+
+        when(boardService.getBoardWithKeyAndPassword(anyString(), anyString())).thenReturn(initialBoard);
+        when(boardService.saveBoard(any(Board.class))).thenReturn(finalBoard);
+
+        // Perform the request
+        this.mockMvc.perform(post("/columns/joinkey/remove/Column 1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString("password")))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(toBeRemovedColumn)));
+
+        assertEquals(0, initialBoard.getColumns().size());
+        assertEquals(initialBoard, finalBoard);
+    }
 }
