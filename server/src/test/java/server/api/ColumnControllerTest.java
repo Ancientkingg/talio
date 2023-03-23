f305deb85dbadf5f375b999c7160bcb18162a751
Tejas Kochar <T.Kochar@student.tudelft.nl>
2023-03-23 09:02:23 +0100

    tested addColumn method for a Board with a password
    (YES, IT FINALLY WORKS :)))))))

--- server/src/test/java/server/api/ColumnControllerTest.java
+++ server/src/test/java/server/api/ColumnControllerTest.java
@@ -1,5 +1,6 @@
 package server.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import commons.Board;
 import commons.Column;
@@ -7,26 +8,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import server.TestConfig;
-import server.database.BoardRepository;
 import server.services.BoardService;
 
 import java.sql.Timestamp;
-import java.util.SortedSet;
 import java.util.TreeSet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
 
 @WebMvcTest(ColumnController.class)
 public class ColumnControllerTest {
@@ -46,17 +42,6 @@ public class ColumnControllerTest {
         assertNotNull(columnController);
     }
 
-
-    /*
-    * Problem I was running into - Board is null
-    *
-    *
-    * What needs to be done -
-    * getBoardWithKeyAndPassword of BoardService is called by addColumn of ColumnController.
-    */
-
-    // This ones a mess I'll clean it up later
-    // Move to the tests below
     @Test
     public void addColumnToBoardWithPasswordTest() throws Exception {
 
@@ -66,8 +51,8 @@ public class ColumnControllerTest {
         Board finalBoard = new Board("joinkey", "Board 1", "password", new TreeSet<>(), new Timestamp(12345L));
         finalBoard.addColumn(addedColumn);
 
-        when(boardService.getBoardWithKeyAndPassword("joinkey", "password")).thenReturn(initialBoard);
-        when(boardService.saveBoard(finalBoard)).thenReturn(finalBoard);
+        when(boardService.getBoardWithKeyAndPassword(anyString(), anyString())).thenReturn(initialBoard);
+        when(boardService.saveBoard(any(Board.class))).thenReturn(finalBoard);
 
         // Perform the request
         this.mockMvc.perform(post("/columns/joinkey/create/Column 1").param("index", "1")
@@ -75,26 +60,10 @@ public class ColumnControllerTest {
                 .content(objectMapper.writeValueAsString("password")))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(addedColumn)));
+
+        assertEquals(addedColumn, initialBoard.getColumns().first());
+        assertEquals(initialBoard, finalBoard);
     }
 
-//    @Test
-//    public void addColumnToBoardWithoutPasswordTest() throws Exception {
-//
-//        when(boardService.getBoardWithKeyAndPassword("joinkey", "password")).thenReturn(board);
-//        when(boardService.saveBoard(board)).thenReturn(board);
-//
-//        Column columnClientOutput = new Column("Colunn1", 1, new TreeSet<>());
-//
-//        this.mockMvc.perform(post("/columns/joinkey/create/Column1").param("index", "1")
-//                .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString("password")))
-//                .andExpect(status().isOk())
-//                .andExpect(content().json(objectMapper.writeValueAsString(columnClientOutput)));
-//
-//    }
-//
-//    @Test
-//    public void removeColumnFromBoardWithPasswordTest() throws Exception {}
-//
-//    @Test
-//    public void removeColumnFromBoardWithoutPasswordTest() throws Exception {}
+
 }
