4a4e9f58b5deced2a1ed7972ad5075800346291d
Danylo Kozak <D.O.Kozak@student.tudelft.nl>
2023-03-27 13:10:27 +0200

    Attempt

---
+++ docs/agendas/week 7 agenda.md
@@ -0,0 +1,113 @@
+## Week 7 Agenda (Team 73)
+
+This is a template agenda. It gives an overview of what could be in your weekly agenda.
+In the 'Points of action' part you will also find some topics to cover in/after your first meeting.
+
+---
+
+Date:           28/3/2023\
+Main focus:     Reports, ensuring basic criteria for the application are met and making quick progress on advanced features.\
+Chair:          Tejas Kochar\
+Note taker:     Jannes Kelso
+
+# Opening
+Does anyone have something to say before we start?\
+There is a lot that needs to be covered in this meeting
+
+# Approval of the agenda
+Here's what we have lined up today for the meeting:
+- Feedback for the draft HUE Report
+- Draft Product Pitch and Teamwork Self Reflection
+- Review of product progress
+- Work TBD on product this week  
+
+Does anyone want something added to today's agenda?
+
+# Points of action
+
+## Heuristic Usability Evaluation Report Feedback
+
+### The good
+(*Fine*)
+
+### The bad
+(*Minor adjustments required*)
+
+### The ugly
+(*Major changes needed*)
+
+### Other
+(*Anything else*)
+
+
+
+
+## Draft Product Pitch
+
+### General info
+
+### Format
+(*storyboard / video?*)
+
+### Do's
+
+### Don't
+
+### Who will do what?
+(*Will probably be decided in meeting after meeting*)
+
+### Other
+
+
+
+
+## Draft Teamwork Self Reflection
+
+### General
+
+### Advice
+
+### Other
+
+
+
+
+## Product Progress
+
+### Demonstration
+
+
+### Individual reports
+
+#### Danylo
+
+#### Jannes
+
+#### Samuel
+
+#### Rafayel
+
+#### Tejas
+
+### Position wrt basic requirements
+
+
+### Other
+
+
+## Product - expectations for this week
+
+(*High level, will go in depth in meeting after meeting*)
+
+
+# Action points for next week (Summary)
+
+
+# Any other business
+*If anybody has something that should be discussed but came up with that after the agenda was finalized (in point 2), he/she should bring that up now.*
+
+# Question round
+*If there are any questions, now is the time to ask them.*
+
+# Closing
+*Toodle-oo*
---
+++ server/src/test/java/server/api/CardControllerTest.java
@@ -0,0 +1,206 @@
+package server.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import commons.Board;
+import commons.Card;
+import commons.Column;
+import commons.DTOs.CardDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import server.services.BoardService;
+
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CardController.class)
+public class CardControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CardController cardController;
+
+    @MockBean
+    private BoardService boardService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    private Board actualBoard;
+    private Board expectedBoard;
+    private Column actualColumn;
+    private Column expectedColumn;
+
+    @BeforeEach
+    public void setup() { // exectued before each test
+
+        actualBoard = new Board("joinkey", "Board 1", "password", new TreeSet<>(), new Timestamp(12345L));
+        expectedBoard = new Board("joinkey", "Board 1", "password", new TreeSet<>(), new Timestamp(12345L));
+
+        actualColumn = new Column("Column 1", 1, new TreeSet<>());
+        expectedColumn = new Column("Column 1", 1, new TreeSet<>());
+
+        when(boardService.getBoardWithKeyAndPassword(anyString(), anyString())).thenReturn(actualBoard);
+        when(boardService.saveBoard(any(Board.class))).thenReturn(actualBoard);
+    }
+
+    @Test
+    public void contextLoads() {
+        assertNotNull(cardController);
+    }
+
+    @Test
+    public void addCardTest() throws Exception {
+
+        Card cardToBeAdded = new Card("Card 1", 1, "Description", new HashSet<>());
+
+        CardDTO cardDTO = new CardDTO(cardToBeAdded, "password");
+
+        actualBoard.addColumn(actualColumn);
+        expectedBoard.addColumn(expectedColumn);
+
+        expectedColumn.addCard(cardToBeAdded);
+
+        this.mockMvc.perform(post("/cards/add/joinkey/Column 1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(cardDTO)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(cardToBeAdded)));
+
+        assertEquals(expectedBoard, actualBoard);
+    }
+
+    @Test
+    public void removeCardTest() throws Exception {
+
+        Card cardToBeRemoved = new Card("Card 1", 1, "Description", new HashSet<>());
+
+        CardDTO cardDTO = new CardDTO(cardToBeRemoved, "password");
+
+        actualColumn.addCard(cardToBeRemoved);
+
+        actualBoard.addColumn(actualColumn);
+        expectedBoard.addColumn(expectedColumn);
+
+        this.mockMvc.perform(post("/cards/remove/joinkey/Column 1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cardDTO)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(cardToBeRemoved)));
+
+        assertEquals(expectedBoard, actualBoard);
+    }
+
+    @Test
+    public void updateCardTest() throws Exception {
+
+        Card initialCard = new Card("Card 1", 1, "Description", new HashSet<>());
+        Card finalCard = new Card("Card 1", 1, "Changed Description", new HashSet<>());
+
+        CardDTO cardDTO = new CardDTO(finalCard, "password");
+
+        actualColumn.addCard(initialCard);
+        expectedColumn.addCard(finalCard);
+
+        actualBoard.addColumn(actualColumn);
+        expectedBoard.addColumn(expectedColumn);
+
+        this.mockMvc.perform(post("/cards/update/joinkey/Column 1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cardDTO)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(initialCard)));
+
+        assertEquals(expectedBoard, actualBoard);
+    }
+
+    @Test
+    public void updatePositionOfCardTest() throws Exception {
+
+        Card actualCard1 = new Card("Card 1", 0, "Description", new HashSet<>());
+        Card actualCard2 = new Card("Card 2", 1, "Description", new HashSet<>());
+        Card actualCard3 = new Card("Card 3", 2, "Description", new HashSet<>());
+        Card actualCard4 = new Card("Card 4", 3, "Description", new HashSet<>());
+
+        Card expectedCard1 = new Card("Card 1", 2, "Description", new HashSet<>());
+        Card expectedCard2 = new Card("Card 2", 0, "Description", new HashSet<>());
+        Card expectedCard3 = new Card("Card 3", 1, "Description", new HashSet<>());
+        Card expectedCard4 = new Card("Card 4", 3, "Description", new HashSet<>());
+
+        CardDTO cardDTO1 = new CardDTO(actualCard1, "password");
+        CardDTO cardDTO4 = new CardDTO(actualCard4, "password");
+
+        actualColumn.addCard(actualCard1);
+        actualColumn.addCard(actualCard2);
+        actualColumn.addCard(actualCard3);
+        actualColumn.addCard(actualCard4);
+
+        expectedColumn.addCard(expectedCard1);
+        expectedColumn.addCard(expectedCard2);
+        expectedColumn.addCard(expectedCard3);
+        expectedColumn.addCard(expectedCard4);
+
+        actualBoard.addColumn(actualColumn);
+        expectedBoard.addColumn(expectedColumn);
+
+        // 1, 2, 3, 4 -> 2, 3, 1, 4
+
+        // weird thing about Column.updateCardPostion - passing position 4 as new index results in card being placed at 3rd position
+        this.mockMvc.perform(post("/cards/updatePosition/joinkey/Column 1/4")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cardDTO1)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(actualColumn)));
+
+        assertEquals(expectedBoard, actualBoard);
+
+        int[] expectedPriorities = {0, 2, 3, 1};
+
+        int i = 0;
+
+        for(Card c : expectedColumn.getCards()) {
+            c.setPriority(expectedPriorities[i ++]);
+        }
+
+        // 2, 3, 1, 4 -> 2, 4, 3, 1
+        this.mockMvc.perform(post("/cards/updatePosition/joinkey/Column 1/3")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cardDTO4)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(actualColumn)));
+
+        assertEquals(expectedBoard, actualBoard);
+
+        expectedPriorities =  new int[] {1, 3, 0, 2};
+
+        i = 0;
+
+        for(Card c : expectedColumn.getCards()) {
+            c.setPriority(expectedPriorities[i ++]);
+        }
+
+        // 2, 3, 1, 4 -> 1, 2, 4, 3
+        this.mockMvc.perform(post("/cards/updatePosition/joinkey/Column 1/2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cardDTO1)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(actualColumn)));
+
+        assertEquals(expectedBoard, actualBoard);
+    }
+}
