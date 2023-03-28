b8e9543a5cf31f19b97abbb37a73fdd128858875
RafayelGardishyan <r.v.gardishyan@student.tudelft.nl>
2023-03-28 23:19:42 +0200

    Fixed the empty column bug

--- client/src/main/java/client/scenes/CreateColumnCtrl.java
+++ client/src/main/java/client/scenes/CreateColumnCtrl.java
@@ -38,10 +38,11 @@ public class CreateColumnCtrl {
      * Will be used to create a column when user passes through the column name
      */
     public void createColumn() throws BoardChangeException {
-        final Column column = new Column(columnName.getText(), demoIndexCounter++, new TreeSet<>());
+        final Column column = new Column(columnName.getText().isEmpty() ? columnName.getPromptText() : columnName.getText(), demoIndexCounter++, new TreeSet<>());
         boardService.addColumnToCurrentBoard(column);
         mainCtrl.showOverview();
         mainCtrl.refreshOverview();
+        columnName.setPromptText(getFunColumnName());
     }
 
     /**
@@ -49,5 +50,31 @@ public class CreateColumnCtrl {
      */
     public void clearFields() {
         columnName.clear();
+    }
+
+    /**
+     * Generates a random column name
+     * @return a fun column name
+     */
+    private String getFunColumnName() {
+        final String[] adjectives = { "fun", "cool", "awesome", "great", "amazing", "wonderful", "fantastic",
+                                        "incredible", "magnificent", "marvelous", "spectacular", "superb",
+                                        "super", "fabulous", "fab", "excellent", "excellent", "terrific", "terrific",
+                                        "wicked", "wicked"};
+
+        final String[] animalNames = {"dog", "cat", "fish", "bird", "hamster", "rabbit", "turtle", "snake", "lizard",
+                                      "frog", "toad", "salamander", "chameleon", "gecko", "iguana", "alligator",
+                                      "crocodile", "lizard", "snake", "turtle", "tortoise", "shark", "whale",
+                                      "dolphin", "seal", "otter", "bear", "panda", "monkey", "gorilla", "ape",
+                                      "chimpanzee", "orangutan", "gibbon", "lemur", "squirrel", "chipmunk",
+                                      "mouse", "rat", "rabbit", "hare", "deer", "elk", "moose", "buffalo", "cow",
+                                      "bull", "horse", "pony", "zebra", "giraffe", "rhinoceros", "hippopotamus",
+                                      "elephant", "camel", "llama", "alpaca", "sheep", "goat", "pig", "boar",
+                                      "wolf", "fox", "coyote", "dog", "cat", "lion", "tiger", "leopard", "cheetah",
+                                      "jaguar", "panther", "hyena", "bear", "panda", "koala", "kangaroo", "wallaby",
+                                      "wombat", "opossum", "platypus", "kookaburra", "emu"};
+
+        return adjectives[(int) (Math.random() * adjectives.length)]
+                + " " + animalNames[(int) (Math.random() * animalNames.length)];
     }
 }
