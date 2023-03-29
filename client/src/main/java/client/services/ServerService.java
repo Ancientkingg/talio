9ef069e3c3d6dac7f2a9419ba5e57495c47481b1
Tejas Kochar <T.Kochar@student.tudelft.nl>
2023-03-29 13:37:04 +0200

    Change usages in client side

--- client/src/main/java/client/services/ServerService.java
+++ client/src/main/java/client/services/ServerService.java
@@ -117,7 +117,7 @@ public class ServerService {
                     .path("/columns")
                     .path("/remove")
                     .path(board.getJoinKey())
-                    .path(column.getHeading())
+                    .path(String.valueOf(column.getId()))
                     .request(APPLICATION_JSON)
                     .post(Entity.entity(board.getPassword(), APPLICATION_JSON), Column.class);
         }
@@ -136,7 +136,7 @@ public class ServerService {
                     .path("/cards")
                     .path("/add")
                     .path(board.getJoinKey())
-                    .path(column.getHeading())
+                    .path(String.valueOf(column.getId()))
                     .request(APPLICATION_JSON)
                     .post(Entity.entity(new CardDTO(card, board.getPassword()), APPLICATION_JSON), Card.class);
         }
@@ -155,7 +155,7 @@ public class ServerService {
                     .path("/cards")
                     .path("/remove")
                     .path(board.getJoinKey())
-                    .path(column.getHeading())
+                    .path(String.valueOf(column.getId()))
                     .request(APPLICATION_JSON)
                     .post(Entity.entity(new CardDTO(card, board.getPassword()), APPLICATION_JSON), Card.class);
         }
