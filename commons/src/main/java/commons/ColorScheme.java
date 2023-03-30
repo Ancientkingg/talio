90ea544708e4c6a4b84fbfcec1313fd9c2110a37
Tejas Kochar <T.Kochar@student.tudelft.nl>
2023-03-30 17:30:58 +0200

    Add ColorScheme entity

---
+++ commons/src/main/java/commons/ColorScheme.java
@@ -0,0 +1,63 @@
+package commons;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.awt.*;
+import java.util.Objects;
+
+@Entity
+public class ColorScheme {
+
+    @Id @Getter @Setter
+    Color textColor;
+
+    @Id @Getter @Setter
+    Color backgroundColor;
+
+    /**
+     * Constructor for ColorScheme
+     * @param textColor color for text (called font color in backlog)
+     * @param backgroundColor color for background
+     */
+    public ColorScheme(final Color textColor, final Color backgroundColor) {
+        this.textColor = textColor;
+        this.backgroundColor = backgroundColor;
+    }
+
+    /**
+     * for checkstyle
+     */
+    public ColorScheme() {
+
+    }
+
+    /**
+     * Equals method for ColorScheme
+     * @param o to compare with this
+     * @return this == o ?
+     */
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final ColorScheme that = (ColorScheme) o;
+
+        if (!Objects.equals(textColor, that.textColor)) return false;
+        return Objects.equals(backgroundColor, that.backgroundColor);
+    }
+
+    /**
+     * hashcode for ColorScheme
+     * @return hash code
+     */
+    @Override
+    public int hashCode() {
+        int result = textColor != null ? textColor.hashCode() : 0;
+        result = 31 * result + (backgroundColor != null ? backgroundColor.hashCode() : 0);
+        return result;
+    }
+}
