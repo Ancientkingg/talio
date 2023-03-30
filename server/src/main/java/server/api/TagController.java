15d5bf781d65ec360c43b6a742552aedae497cc0
RafayelGardishyan <r.v.gardishyan@student.tudelft.nl>
2023-03-30 16:18:06 +0200

    Added TagController

--- server/src/main/java/server/api/TagController.java
+++ server/src/main/java/server/api/TagController.java
@@ -26,18 +26,154 @@ public class TagController {
     private final Logger logger = LogManager.getLogger(CardController.class);
 
 
+    /**
+     * Constructor for the Card Controller
+     * @param boardService Dependency injection for the board service
+     * @param messagingTemplate Template to send updates over socket
+     */
     public TagController(final BoardService boardService, final SimpMessageSendingOperations messagingTemplate) {
         this.boardService = boardService;
         this.messagingTemplate = messagingTemplate;
     }
 
+    /**
+     * Adds a tag to a board
+     * @param tagDTO Tag to be added
+     * @param joinKey Key used to identify board to which tag is to be added
+     *
+     * @return The tag added to board
+     */
     @PostMapping("/add/{joinKey}/")
     public ResponseEntity<Tag> addCard(@Valid @RequestBody final TagDTO tagDTO,
                                        @PathVariable final String joinKey)
     {
         final String password = tagDTO.password();
-        final Board board = boardService.getBoardWithKey(joinKey);
+        final Board board = boardService.getBoardWithKeyAndPassword(joinKey, password);
         final Tag tag = tagDTO.tag();
         board.addTag(tag);
+        boardService.saveBoard(board);
+
+        updateTagAdded(tag, board);
+        return ResponseEntity.ok(tag);
+    }
+
+    /**
+     * Removes a tag from a board
+     * @param tagDTO Tag to be removed
+     * @param joinKey Key used to identify board from which tag is to be removed
+     *
+     * @return The tag removed from board
+     */
+    @PostMapping("/remove/{joinKey}/")
+    public ResponseEntity<Tag> removeTag(@Valid @RequestBody final TagDTO tagDTO ,
+                                         @PathVariable final String joinKey)
+    {
+        final String password = tagDTO.password();
+        final Board board = boardService.getBoardWithKeyAndPassword(joinKey, password);
+        final Tag tag = tagDTO.tag();
+
+        board.deleteTag(tag);
+        boardService.saveBoard(board);
+
+        updateTagRemoved(tag, board);
+        return ResponseEntity.ok(tag);
+    }
+
+    /**
+     * Updates a tag on a board
+     * @param tagDTO Tag to be updated
+     * @param joinKey Key used to identify board on which tag is to be updated
+     *
+     * @return The tag updated on board
+     */
+    @PostMapping("/update/{joinKey}/")
+    public ResponseEntity<Tag> updateTag(@Valid @RequestBody final TagDTO tagDTO ,
+                                         @PathVariable final String joinKey)
+    {
+        final String password = tagDTO.password();
+        final Board board = boardService.getBoardWithKeyAndPassword(joinKey, password);
+        final Tag tag = tagDTO.tag();
+
+        board.updateTag(tag);
+
+        boardService.saveBoard(board);
+
+        updateTagUpdated(tag, board);
+        return ResponseEntity.ok(tag);
+    }
+
+    /**
+     * Adds a tag to a card
+     * @param tagDTO Tag to be added
+     * @param joinKey Key used to identify board to which tag is to be added
+     * @param cardId Id of card to which tag is to be added
+     *
+     * @return The tag added to card
+     */
+    @PostMapping("/addToCard/{joinKey}/{cardId}")
+    public ResponseEntity<Tag> addTagToCard(@Valid @RequestBody final TagDTO tagDTO,
+                                            @PathVariable final String joinKey,
+                                            @PathVariable final long cardId)
+    {
+        final String password = tagDTO.password();
+        final Board board = boardService.getBoardWithKeyAndPassword(joinKey, password);
+        final Tag tag = tagDTO.tag();
+
+        board.addTagToCard(cardId, tag);
+        boardService.saveBoard(board);
+
+        updateTagAddedToCard(tag, cardId, board);
+        return ResponseEntity.ok(tag);
+    }
+
+    /**
+     * Removes a tag from a card
+     * @param tagDTO Tag to be removed
+     * @param joinKey Key used to identify board from which tag is to be removed
+     * @param cardId Id of card from which tag is to be removed
+     *
+     * @return The tag removed from card
+     */
+    @PostMapping("/removeFromCard/{joinKey}/{cardId}")
+    public ResponseEntity<Tag> removeTagFromCard(@Valid @RequestBody final TagDTO tagDTO,
+                                                 @PathVariable final String joinKey,
+                                                 @PathVariable final long cardId)
+    {
+        final String password = tagDTO.password();
+        final Board board = boardService.getBoardWithKeyAndPassword(joinKey, password);
+        final Tag tag = tagDTO.tag();
+
+        board.removeTagFromCard(cardId, tag);
+        boardService.saveBoard(board);
+
+        updateTagRemovedFromCard(tag, cardId, board);
+        return ResponseEntity.ok(tag);
+    }
+
+    private void updateTagRemovedFromCard(final Tag tag, final long cardId, final Board board) {
+        logger.info("Tag removed from card, propagating: " + board.getJoinKey() + " with name: " + tag.getTitle());
+        messagingTemplate.convertAndSend("/topic/boards/" + board.getJoinKey() + "/cards/"
+                + cardId + "/removeTag", tag);
+    }
+
+    private void updateTagAddedToCard(final Tag tag, final long cardId, final Board board) {
+        logger.info("Tag added to card, propagating: " + board.getJoinKey() + " with name: " + tag.getTitle());
+        messagingTemplate.convertAndSend("/topic/boards/" + board.getJoinKey() + "/cards/"
+                + cardId + "/editTag", tag);
+    }
+
+    private void updateTagUpdated(final Tag tag, final Board board) {
+        logger.info("Tag updated in board, propagating: " + board.getJoinKey() + " with name: " + tag.getTitle());
+        messagingTemplate.convertAndSend("/topic/boards/" + board.getJoinKey() + "/addTag", tag);
+    }
+
+    private void updateTagRemoved(final Tag tag, final Board board) {
+        logger.info("Tag removed from board, propagating: " + board.getJoinKey() + " with name: " + tag.getTitle());
+        messagingTemplate.convertAndSend("/topic/boards/" + board.getJoinKey() + "/removeTag", tag);
+    }
+
+    private void updateTagAdded(final Tag tag, final Board board) {
+        logger.info("Tag added to board, propagating: " + board.getJoinKey() + " with name: " + tag.getTitle());
+        messagingTemplate.convertAndSend("/topic/tags/" + board.getJoinKey() + "/editTag", tag);
     }
 }
