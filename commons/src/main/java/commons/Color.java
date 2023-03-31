adef0b20f174867240016e3a2c9b864a4bc46159
Samuel Bruin <s.bruin-1@student.tudelft.nl>
2023-03-31 21:51:35 +0200

    Fix tests and checkstyle

--- commons/src/main/java/commons/Color.java
+++ commons/src/main/java/commons/Color.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Color {
@@ -31,7 +32,14 @@ public class Color {
     @Setter
     private int alpha;
 
-    public Color(int red, int green, int blue, int alpha) {
+    /**
+     * Constructor for Color
+     * @param red red value
+     * @param green green value
+     * @param blue blue value
+     * @param alpha alpha value
+     */
+    public Color(final int red, final int green, final int blue, final int alpha) {
         this.red = red;
         this.green = green;
         this.blue = blue;
@@ -43,5 +51,18 @@ public class Color {
      */
     protected Color() {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Color color = (Color) o;
+        return red == color.red && green == color.green && blue == color.blue && alpha == color.alpha;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(red, green, blue, alpha);
     }
 }
--- commons/src/test/java/commons/TagTest.java
+++ commons/src/test/java/commons/TagTest.java
@@ -10,14 +10,14 @@ class TagTest {
 
     @Test
     void testConstructor() {
-        Tag t = new Tag("Important", new ColorScheme(Color.black, Color.white));
+        Tag t = new Tag("Important", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
         assertEquals("Important", t.getTitle());
-        assertEquals(new ColorScheme(Color.black, Color.white), t.getColorScheme());
+        assertEquals(new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)), t.getColorScheme());
     }
 
     @Test
     void setTitle() {
-        Tag t = new Tag("Important", new ColorScheme(Color.black, Color.white));
+        Tag t = new Tag("Important", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
         assertEquals("Important", t.getTitle());
         t.setTitle("Priority");
         assertEquals("Priority", t.getTitle());
@@ -25,30 +25,30 @@ class TagTest {
 
     @Test
     void setHexColor() {
-        Tag t = new Tag("Important", new ColorScheme(Color.black, Color.white));
+        Tag t = new Tag("Important", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
         assertEquals("Important", t.getTitle());
-        t.setColorScheme(new ColorScheme(Color.black, Color.red));
-        assertEquals(new ColorScheme(Color.black, Color.red), t.getColorScheme());
+        t.setColorScheme(new ColorScheme(new Color(0,0,0,255), new Color(255,0,0,255)));
+        assertEquals(new ColorScheme(new Color(0,0,0,255), new Color(255,0,0,255)), t.getColorScheme());
     }
 
     @Test
     void testEquals() {
-        Tag t = new Tag("Important", new ColorScheme(Color.black, Color.white));
-        Tag t2 = new Tag("Important", new ColorScheme(Color.black, Color.white));
+        Tag t = new Tag("Important", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
+        Tag t2 = new Tag("Important", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
         assertEquals(t, t2);
-        Tag t3 = new Tag("Priority", new ColorScheme(Color.black, Color.white));
-        Tag t4 = new Tag("Important", new ColorScheme(Color.red, Color.white));
+        Tag t3 = new Tag("Priority", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
+        Tag t4 = new Tag("Important", new ColorScheme(new Color(255,0,0,255), new Color(255,255,255,255)));
         assertNotEquals(t, t3);
         assertNotEquals(t, t4);
     }
 
     @Test
     void testHashCode() {
-        Tag t = new Tag("Important", new ColorScheme(Color.black, Color.white));
-        Tag t2 = new Tag("Important", new ColorScheme(Color.black, Color.white));
+        Tag t = new Tag("Important", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
+        Tag t2 = new Tag("Important", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
         assertEquals(t.hashCode(), t2.hashCode());
-        Tag t3 = new Tag("Priority", new ColorScheme(Color.black, Color.white));
-        Tag t4 = new Tag("Important", new ColorScheme(Color.red, Color.white));
+        Tag t3 = new Tag("Priority", new ColorScheme(new Color(0,0,0,255), new Color(255,255,255,255)));
+        Tag t4 = new Tag("Important", new ColorScheme(new Color(255,0,0,255), new Color(255,255,255,255)));
         assertNotEquals(t.hashCode(), t3.hashCode());
         assertNotEquals(t.hashCode(), t4.hashCode());
     }
