258c862388b7ee30dbbfe2ec3ee0c980eae12030
Tejas Kochar <T.Kochar@student.tudelft.nl>
2023-04-03 14:50:34 +0200

    check for usage of 'https://' and throw corresponding error modal if used, if no port number or 'http://' is provided then append them and then use url

--- client/src/main/java/client/scenes/JoinServerCtrl.java
+++ client/src/main/java/client/scenes/JoinServerCtrl.java
@@ -43,6 +43,19 @@ public class JoinServerCtrl {
 
         if (url == null || url.equals(""))
             url = defaultURL;
+        else if (url.startsWith("https://")) {
+            final InfoModal errorModal = new InfoModal(boardService, "Invalid Input",
+                    "Secure URLs are not supported. Use 'http' instead of 'https'", mainCtrl.getCurrentScene());
+            errorModal.showModal();
+            return;
+        }
+
+        if (!url.startsWith("http://"))
+            url = "http://" + url;
+
+        if (!url.matches("/.*:\\d{1,5}/gs"))  // checks if string ends with a colon followed by 1 to 5 digits
+            url = url + "8080"; // if not, add ':8080' as default port number
+
 
         try {
             new URL(url).toURI();
