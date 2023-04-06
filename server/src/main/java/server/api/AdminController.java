5f651f1b2e703be68be5eca3fe50acbb7df7e12b
Tejas Kochar <T.Kochar@student.tudelft.nl>
2023-04-06 14:30:11 +0200

    add endpoint to get all boards in AdminController

--- server/src/main/java/server/api/AdminController.java
+++ server/src/main/java/server/api/AdminController.java
@@ -1,11 +1,16 @@
 package server.api;
 
+import commons.Board;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import server.services.AdminService;
+
+import java.util.List;
 
 import static server.Main.validatePassword;
 
@@ -13,6 +18,16 @@ import static server.Main.validatePassword;
 public class AdminController {
 
     private final Logger logger = LogManager.getLogger(AdminController.class);
+
+    private final AdminService adminService;
+
+    /**
+     * Constructor for AdminController
+     * @param adminService BoardService dependency injection
+     */
+    public AdminController (final AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     /**
      * Verifies password provided by user
@@ -23,5 +38,16 @@ public class AdminController {
     public ResponseEntity<Boolean> verifyPassword (@RequestBody final String password) {
         logger.info("recieved request to switch to admin mode");
         return ResponseEntity.ok(validatePassword(password));
+    }
+
+    /**
+     * gets all boards from the database
+     * @return List of all Boards
+     */
+    @GetMapping("/admin/getAllBoards")
+    public ResponseEntity<List<Board>> getAllBoards () {
+        logger.info("recieved requset to fetch all boards in server");
+        final List<Board> allBoards = adminService.getAllBoards();
+        return ResponseEntity.ok(allBoards);
     }
 }
