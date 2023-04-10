23141e0558d947ed9c9a718ad0d11e514fba9f37
Tejas Kochar <T.Kochar@student.tudelft.nl>
2023-04-10 15:29:21 +0200

    Fix ok button in help modal

--- client/src/main/java/client/scenes/components/modals/ShortcutsHelpModal.java
+++ client/src/main/java/client/scenes/components/modals/ShortcutsHelpModal.java
@@ -3,10 +3,14 @@ package client.scenes.components.modals;
 import client.Main;
 import client.scenes.components.UIComponent;
 import client.services.BoardService;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 public class ShortcutsHelpModal extends Modal implements UIComponent {
 
+    @FXML
+    private Button okButton;
     /**
      * Default constructor for modal
      *
@@ -17,7 +21,12 @@ public class ShortcutsHelpModal extends Modal implements UIComponent {
         super(boardService, parentScene);
 
         loadSource(Main.class.getResource("/components/ShortcutsHelpModal.fxml"));
+
+        okButton.requestFocus();
     }
 
-
+    @FXML
+    private void okButton() {
+        closeModal();
+    }
 }
